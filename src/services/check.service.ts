fcac678a52a602216d1a1d9dfde7fb36c6501346
SEFSIS <sofiagryn@icloud.com>
2023-11-28 15:05:44 +0200

    commit

--- src/services/check.service.ts
+++ src/services/check.service.ts
@@ -1,5 +1,7 @@
+import { User } from "../models/User.model";
 import { checkRepository } from "../repositories/check.repository";
 import { ICheck } from "../types/check.type";
+import { IUser } from "../types/user.type";
 
 class CheckService {
   public async getAll(): Promise<ICheck[]> {
@@ -7,7 +9,22 @@ class CheckService {
   }
 
   public async createCheck(dto: ICheck, userId: string): Promise<ICheck> {
-    return await checkRepository.createCheck(dto, userId);
+    const user: IUser | null = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (user.wallet < dto.price) {
+      throw new Error("Insufficient funds");
+    }
+
+    user.wallet -= dto.price;
+
+    await user.save();
+
+    const createdCheck: ICheck = await checkRepository.createCheck(dto, userId);
+    return createdCheck;
   }
 }
 
